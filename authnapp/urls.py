955ada38be838ee68abc0ab0a830ba6e4540b7db
dan929873 <metallica04@yandex.ru>
2022-01-31 10:43:02 +0300

    lesson_9. URLs as regexp

---
+++ authnapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+
+import authnapp.views as authnapp
+from authnapp.apps import AuthnappConfig
+
+app_name = AuthnappConfig.name
+
+urlpatterns = [
+    re_path(r"^login/$", authnapp.login, name="login"),
+    re_path(r"^logout/$", authnapp.logout, name="logout"),
+    re_path(r"^register/$", authnapp.register, name="register"),
+    re_path(r"^edit/$", authnapp.edit, name="edit"),
+]
